d0a3030b534cf0f7543d4d0869544387e37aa971
Daniel Roe <daniel@roe.dev>
2023-08-24 23:11:31 +0100

    chore: add eslint

--- pages/[username].vue
+++ pages/[username].vue
@@ -1,9 +1,20 @@
 <template>
   <div class="min-h-screen flex flex-col justify-center">
-    <main v-if="commit" class="p-8 m-2 shadow-md max-w-[500px] w-full mx-auto border-[1px] border-gray-600 rounded-md">
+    <main
+      v-if="commit"
+      class="p-8 m-2 shadow-md max-w-[500px] w-full mx-auto border-[1px] border-gray-600 rounded-md"
+    >
       <header class="relative flex flex-row items-center gap-4">
-        <NuxtTime class="absolute font-cal tabular-nums right-0 top-0" :datetime="commit.commit.author.date" year="numeric" />
-        <img class="rounded-full h-16 w-16" :src="commit.author.avatar_url" :alt="`Avatar for ${commit.author.login}`">
+        <NuxtTime
+          class="absolute font-cal tabular-nums right-0 top-0"
+          :datetime="commit.commit.author.date"
+          year="numeric"
+        />
+        <img
+          class="rounded-full h-16 w-16"
+          :src="commit.author.avatar_url"
+          :alt="`Avatar for ${commit.author.login}`"
+        >
         <div class="flex flex-col items-start gap-2">
           <span class="leading-none text-lg font-cal font-semibold">{{ commit.commit.author.name }}</span>
           <span class="leading-none opacity-50 text-sm">@{{ commit.author.login }}</span>
@@ -15,12 +26,19 @@
           <span class="line-clamp-1">{{ commit.commit.message }}</span>
           <span class="text-xs">
             <span class="flex flex-row gap-2">
-              <img class="rounded-full h-4 w-4" :src="commit.repository.owner.avatar_url" :alt="`Avatar for ${commit.repository.owner.login}`">
+              <img
+                class="rounded-full h-4 w-4"
+                :src="commit.repository.owner.avatar_url"
+                :alt="`Avatar for ${commit.repository.owner.login}`"
+              >
               {{ commit.repository.full_name }}
             </span>
           </span>
         </div>
-        <NuxtLink :href="commit.html_url" class="flex-shrink-0 rounded bg-black text-white text-sm shadow px-3 py-2 flex flex-row gap-2 items-center">
+        <NuxtLink
+          :href="commit.html_url"
+          class="flex-shrink-0 rounded bg-black text-white text-sm shadow px-3 py-2 flex flex-row gap-2 items-center"
+        >
           See full commit
         </NuxtLink>
       </div>
