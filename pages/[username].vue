de2cfa344072db8dd2c459712952e5172457c253
Daniel Roe <daniel@roe.dev>
2023-08-24 22:28:15 +0100

    fix: use `/search/commits` endpoint

--- pages/[username].vue
+++ pages/[username].vue
@@ -1,22 +1,20 @@
 <template>
-  <div class="min-h-screen grid place-content-center">
-    <main v-if="event" class="p-8 m-2 shadow-md max-w-[500px] w-full mx-auto border-[1px] border-gray-600 rounded-md">
+  <div class="min-h-screen flex flex-col justify-center">
+    <main v-if="commit" class="p-8 m-2 shadow-md max-w-[500px] w-full mx-auto border-[1px] border-gray-600 rounded-md">
       <header class="flex flex-row items-center gap-4">
-        <img class="rounded-full h-16 w-16" :src="event.actor.avatar_url" :alt="`Avatar for ${event.actor.display_login}`">
+        <img class="rounded-full h-16 w-16" :src="commit.author.avatar_url" :alt="`Avatar for ${commit.author.login}`">
         <div class="flex flex-col items-start gap-2">
-          <span class="leading-none text-lg font-cal font-semibold">{{ event.payload.commits[0].author.name }}</span>
-          <span class="leading-none opacity-50 text-sm">{{ event.actor.display_login }}</span>
+          <span class="leading-none text-lg font-cal font-semibold">{{ commit.commit.author.name }}</span>
+          <span class="leading-none opacity-50 text-sm">@{{ commit.author.login }}</span>
         </div>
       </header>
       <hr class="my-4">
-      <ul class="flex flex-col gap-1 py-2">
-        <li v-for="commit in event.payload.commits || []" class="flex flex-row justify-between items-start gap-4">
-          <span class="font-sans leading-none whitespace-pre-wrap">{{ commit.message }}</span>
-          <NuxtLink :href="commit.url" class="rounded bg-black text-white shadow px-3 py-2">
-            See it on GitHub
-          </NuxtLink>
-        </li>
-      </ul>
+      <div class="flex flex-row justify-between items-center gap-4">
+        <span class="font-sans leading-none whitespace-pre-wrap">{{ commit.commit.message }}</span>
+        <NuxtLink :href="commit.html_url" class="rounded bg-black text-white text-sm shadow px-3 py-2">
+          See it on GitHub
+        </NuxtLink>
+      </div>
     </main>
   </div>
 </template>
@@ -26,7 +24,7 @@ import 'cal-sans'
 const route = useRoute('username')
 const username = route.params.username
 
-const { data: event } = await useFetch(`/api/${username}`)
+const { data: commit } = await useFetch(`/api/${username}`)
 
 // TODO: show OG image with commit/PR metadata
 </script>
--- server/api/[username].get.ts
+++ server/api/[username].get.ts
@@ -2,64 +2,48 @@ import * as v from 'valibot'
 import type { Output } from 'valibot'
 import type { EventHandler, H3Event } from 'h3'
 
-export default defineLazyEventHandler(async () => {
-  const config = useRuntimeConfig()
+export default defineCachedEventHandler(async event => {
+  const config = useRuntimeConfig(event)
+  const username = getRouterParam(event as H3Event<any>, 'username')
+  if (!username || !username.match(/^[\w\d]+$/)) throw createError({ message: 'username is required' })
 
-  const $gh = $fetch.create({
+  const results = await $fetch('/search/commits', {
     baseURL: 'https://api.github.com',
     headers: {
       'Authorization': `Bearer ${config.github.token}`,
       'X-GitHub-Api-Version': '2022-11-28',
       Accept: 'application/vnd.github+json'
+    },
+    query: {
+      q: `author:${username}`,
+      order: 'asc',
+      sort: 'committer-date',
+      per_page: 1
     }
-  })
+  }).then(r => ResultsSchema._parse(r).output)
+  const firstCommit = results?.items[0]
+  if (!firstCommit) throw createError({ statusCode: 404, message: 'no commits found' })
 
-  async function getEvents(username: string) {
-    const events = []
-    let page = 1
-    let data: Array<Output<typeof ghEvent>>
-    do {
-      data = await $gh(`/users/${username}/events`, {
-        query: {
-          per_page: 100,
-          page
-        },
-      })
-      // events.push(...data.map(e => ghEvent._parse(e).output).filter(Boolean))
-      events.push(...data)
-      page++
-    } while (data?.length)
-    return events
-  }
+  return firstCommit
+}) as EventHandler<{}, Output<typeof ResultsSchema>>
 
-  return defineCachedEventHandler(async event => {
-    const username = getRouterParam(event as H3Event<any>, 'username')
-    if (!username) throw createError({ message: 'username is required' })
-
-    const events = await getEvents(username)
-    const oldestCommit = events.filter(e => e?.type === 'PushEvent' || e?.type === 'PullRequestEvent').sort((a, b) => new Date(a!.created_at).valueOf() - new Date(b!.created_at).valueOf())[0]
-
-    return oldestCommit
-  }) as EventHandler<{}, Output<typeof ghEvent>>
-})
-
-const ghEvent = v.object({
-  type: v.string(),
-  actor: v.object({
-    login: v.string(),
-    avatar_url: v.string(),
-    display_login: v.string()
-  }),
-  repo: v.object({
-    name: v.string(),
-    url: v.string()
-  }),
-  created_at: v.string(),
-  payload: v.object({
-    commits: v.array(v.object({
+const ResultsSchema = v.object({
+  total_count: v.number(),
+  items: v.array(v.object({
+    html_url: v.string(),
+    commit: v.object({
       message: v.string(),
-      sha: v.string(),
-      url: v.string()
-    }))
-  })
+      author: v.object({
+        date: v.string(),
+        name: v.string(),
+        email: v.string(),
+      }),
+    }),
+    author: v.object({
+      login: v.string(),
+      avatar_url: v.string(),
+      html_url: v.string(),
+    })
+  }))
 })
+
