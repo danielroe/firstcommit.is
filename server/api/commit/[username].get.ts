325fcbcdb79bb03ec11203503fe1fd021e0841f4
Daniel Roe <daniel@roe.dev>
2023-08-26 22:41:07 +0100

    feat: use github og image for now

--- server/api/commit/[username].get.ts
+++ server/api/commit/[username].get.ts
@@ -20,16 +20,31 @@ export default defineCachedEventHandler(async event => {
       per_page: 1
     }
   })
-  const firstCommit = ResultsSchema._parse(results).output?.items[0]
-  if (!firstCommit) {
+  const commit = ResultsSchema._parse(results).output?.items[0]
+  if (!commit) {
     // @ts-expect-error unknown
     if (results?.total_count) {
       console.log(JSON.stringify(ResultsSchema._parse(results).issues))
     }
     throw createError({ statusCode: 404, message: 'no commits found' })
   }
+  const html = await $fetch<string>(commit.html_url)
+  const ogImage = html.match(/<meta property="og:image" content="([^"]+)"/)?.[1]
 
-  return firstCommit
+  return {
+    ogImage,
+    date: commit.commit.author.date,
+    avatar: commit.author.avatar_url,
+    link: commit.html_url,
+    message: commit.commit.message,
+    username: commit.author.login,
+    author: commit.commit.author.name,
+    org: {
+      avatar: commit.repository.owner.avatar_url,
+      name: commit.repository.owner.login,
+      repository: commit.repository.full_name
+    }
+  }
 })
 
 const ResultsSchema = v.object({
