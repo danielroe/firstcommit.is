7d0b5e5b29582f249b2843e0cc6669f6f9e651a0
Daniel Roe <daniel@roe.dev>
2023-09-01 10:38:21 +0100

    feat: add better nav on commit page

--- pages/[username].vue
+++ pages/[username].vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-grow flex flex-col justify-center p-2">
+  <div class="flex-grow flex flex-col justify-center p-2 gap-4">
     <main
       v-if="commit"
       class="p-4 md:p-8 shadow-md max-w-[500px] w-full mx-auto border-[1px] border-gray-600 rounded-md"
@@ -58,13 +58,51 @@
         </div>
       </div>
     </main>
-    <nav class="flex flex-row justify-center">
+    <nav class="flex flex-row gap-2 justify-center max-w-[500px] w-full mx-auto">
       <NuxtLink
-        class="p-4 text-sm"
-        href="/"
+        class="rounded flex-shrink-0 bg-black text-white text-sm shadow px-3 py-2 flex flex-row gap-2 items-center"
+        href="/connect/github"
+        external
       >
-        &laquo; go back
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-4 w-4"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M12.001 2c-5.525 0-10 4.475-10 10a9.994 9.994 0 0 0 6.837 9.488c.5.087.688-.213.688-.476c0-.237-.013-1.024-.013-1.862c-2.512.463-3.162-.612-3.362-1.175c-.113-.288-.6-1.175-1.025-1.413c-.35-.187-.85-.65-.013-.662c.788-.013 1.35.725 1.538 1.025c.9 1.512 2.337 1.087 2.912.825c.088-.65.35-1.087.638-1.337c-2.225-.25-4.55-1.113-4.55-4.938c0-1.088.387-1.987 1.025-2.688c-.1-.25-.45-1.275.1-2.65c0 0 .837-.262 2.75 1.026a9.28 9.28 0 0 1 2.5-.338c.85 0 1.7.112 2.5.337c1.913-1.3 2.75-1.024 2.75-1.024c.55 1.375.2 2.4.1 2.65c.637.7 1.025 1.587 1.025 2.687c0 3.838-2.337 4.688-4.563 4.938c.363.312.676.912.676 1.85c0 1.337-.013 2.412-.013 2.75c0 .262.188.574.688.474A10.016 10.016 0 0 0 22 12c0-5.525-4.475-10-10-10Z"
+          />
+        </svg>
+        Find yours
       </NuxtLink>
+      <form
+        class="text-sm flex flex-row gap-2 border-black border-2 rounded-md shadow-sm leading-none w-full overflow-hidden justify-stretch"
+        @submit.prevent="openCommit"
+      >
+        <input
+          v-model="newUsername"
+          type="text"
+          class="pl-2 py-2 lowercase flex-1"
+          :placeholder="username"
+        >
+        <button
+          :disabled="!username.trim()"
+          type="submit"
+          class="my-1 mr-1 flex-shrink-0 rounded border-transparent border-2 bg-black text-white 
+          hover:bg-white hover:text-black hover:border-black shadow transition-colors p-1"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-4 h-4"
+            viewBox="0 0 24 24"
+          ><path
+            fill="currentColor"
+            d="M5 14V5h2v7h10.172l-3.95-3.95l1.414-1.414L21 13l-6.364 6.364l-1.414-1.414l3.95-3.95H5Z"
+          /></svg>
+          <span class="sr-only">Find first commit</span>
+        </button>
+      </form>
     </nav>
   </div>
 </template>
@@ -84,6 +122,11 @@ definePageMeta({
 
 const route = useRoute('username')
 const username = route.params.username
+
+const newUsername = ref('')
+function openCommit () {
+  return navigateTo(`/${newUsername.value.toLowerCase()}`)
+}
 
 const { data: commit } = await useFetch(`/api/commit/${username}`)
 
