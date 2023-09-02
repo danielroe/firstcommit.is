8f05fd9a925a8b281c616f4d697bd9aad4a6cefd
Felix <felix.bohlin@protonmail.com>
2023-09-02 22:14:39 +0200

    Add viewport metadata to prevent page zoom on mobile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,6 @@ const resolver = createResolver(import.meta.url)
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'firstcommit - find your first commit',
-      charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0',
     },
   },
